760b38b39fd7fb27cfae50089982637e502a3863
Vulpiro <vulpiroo@gmail.com>
2023-12-14 16:01:53 +0100

    Marek HabitObj class created with basic variables

---
+++ ZPSB_SPAGETTI/Habit/HabitObj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPSB_SPAGETTI.Habit
+{
+    class HabitObj
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime habitTime { get; set; }
+        public int damage { get; set; }
+
+        public HabitObj()
+        {
+
+        }
+    }
+}
