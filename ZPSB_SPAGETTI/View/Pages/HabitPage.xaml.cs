09e2c7bbe7221e2093f6080130d266ffea041fd4
Vulpiro <vulpiroo@gmail.com>
2024-01-18 09:41:00 +0100

    Marek is created a lot of things

---
+++ ZPSB_SPAGETTI/View/Pages/HabitPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZPSB_SPAGETTI.View.Pages
+{
+    /// <summary>
+    /// Interaction logic for HabitPage.xaml
+    /// </summary>
+    public partial class HabitPage : Page
+    {
+        public HabitPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
