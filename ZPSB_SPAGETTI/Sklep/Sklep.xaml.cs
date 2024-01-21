13ea6a8f4fe1dc778fe825ba91b4a6e31273d9e1
lucja-t <153625572+lucja-t@users.noreply.github.com>
2024-01-21 16:36:57 +0100

    Nowy branch z oknem sklepu i przedmiotami

---
+++ ZPSB_SPAGETTI/Sklep/Sklep.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZPSB_SPAGETTI.Sklep
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Sklep.xaml
+    /// </summary>
+    public partial class Sklep : Window
+    {
+        public Sklep()
+        {
+            InitializeComponent();
+        }
+    }
+}
